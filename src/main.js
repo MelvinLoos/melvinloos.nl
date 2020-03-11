62a6a8e47360c9ff637052ae83ae71d3be17824e
Melvin Loos <melvin@melvinloos.nl>
2020-03-11 22:59:51 +0100

    Fix: contact form is not shown

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
-import { NavPlugin, NavbarPlugin, FormPlugin, ButtonPlugin } from 'bootstrap-vue'
+import { NavPlugin, NavbarPlugin, FormPlugin, FormGroupPlugin, FormInputPlugin, FormTextareaPlugin, ButtonPlugin } from 'bootstrap-vue'
 import './assets/styles/main.scss'
 
 import checkIfMobile from './mixins/checkIfMobile'
@@ -9,6 +9,9 @@ export default function (Vue, { router, head, isClient }) {
   Vue.use(NavPlugin)
   Vue.use(NavbarPlugin)
   Vue.use(FormPlugin)
+  Vue.use(FormGroupPlugin)
+  Vue.use(FormInputPlugin)
+  Vue.use(FormTextareaPlugin)
   Vue.use(ButtonPlugin)
   Vue.component('Layout', DefaultLayout)
   Vue.mixin(checkIfMobile)
