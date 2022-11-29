0f79f294ea54ea506b2382012f8deb04b72d8747
Melvin Loos <melvin@melvinloos.nl>
2022-11-29 12:47:58 +0100

    Update Blog.vue

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,23 +2,25 @@
   <Layout>
     <h1 class="my-4 mb-5">Blog</h1>
 
-    <g-link
-      v-if="item.node.path"
-      :to="item.node.path"
+    <template 
       v-for="item in $page.posts.edges"
-      :key="item.node.id"
-      class="blog-post"
-    >
+      :key="item.node.id">
+      <g-link
+        v-if="item.node.path"
+        :to="item.node.path"
 
-			<div class="media my-5">
-				<g-image immediate :src="item.node.image" class="mr-3" alt="image" />
-				<div class="media-body">
-					<h5 class="mt-0">{{item.node.title}}</h5>
-					<p class="text-dark">{{item.node.excerpt}}</p>
-				</div>
-			</div>
+        class="blog-post">
 
-    </g-link>
+        <div class="media my-5">
+          <g-image immediate :src="item.node.image" class="mr-3" alt="image" />
+          <div class="media-body">
+            <h5 class="mt-0">{{item.node.title}}</h5>
+            <p class="text-dark">{{item.node.excerpt}}</p>
+          </div>
+        </div>
+
+      </g-link>
+    </template>
   </Layout>
 </template>
 
